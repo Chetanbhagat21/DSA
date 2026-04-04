33167c3d7d877217a3f7e32a1230ad8344c6b82a
Chetan Bhagat <cbhagat191@gmail.com>
2026-04-04 20:44:45 +0530

    Add files via upload

---
+++ Day 1 Basics.py
@@ -0,0 +1,29 @@
+# 1 variables & data types 
+a = 10
+b=3.14
+c="hello"
+d=True
+# type 
+print(type(d))
+
+# 2 input & output
+name = input("enter your name :")
+print("hello",name)
+num = int(input("enter age : "))
+
+#3 type casting 
+a="10"
+b=int(a)
+
+x=5
+y=float(x)
+
+# 4 basic operators 
+a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
