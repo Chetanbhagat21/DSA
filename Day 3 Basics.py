8307e511cfc813f769eeb8630de36035810a399f
Chetan Bhagat <cbhagat191@gmail.com>
2026-04-07 20:39:56 +0530

    Add files via upload

---
+++ Day 3 Basics.py
@@ -0,0 +1,23 @@
+# for loop 
+
+for i in range(5):
+    print(i)
+    
+# while loop 
+
+i = 1 
+while i <=5:
+    print(i)
+    i+=1
+    
+# break & continue
+
+for i in range(5):
+    if i==3:
+        break
+    print(i)
+    
+for i in range(5):
+    if i == 3:
+        continue
+    print(i)
