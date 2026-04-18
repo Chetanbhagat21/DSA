145250f1bfd63d01dbd9e225121381e0c8cb1da6
Chetan Bhagat <cbhagat191@gmail.com>
2026-04-18 19:58:36 +0530

    Add files via upload

---
+++ Day 5 Basics.py
@@ -0,0 +1,21 @@
+# lists
+
+nums=[1,2,3,4]
+#operations
+nums.append(5)
+nums.remove(2)
+nums.pop()
+nums.sort()
+
+# sets 
+
+s={1,2,2,3}
+print(s)
+
+# dictionary 
+
+d={
+    "a":1,
+    "b":2
+}
+print(d["a"])
